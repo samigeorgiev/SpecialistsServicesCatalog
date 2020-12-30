33019bd78c0c0c1666ed23665509fdfcec2b9787
Samuil Georgiev <sami.georgiev@gmail.com>
2020-12-30 21:34:25 +0200

    Make Props not exported + remove empty Props (#3)

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -2,9 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { Header } from '../Header';
 import styles from './Layout.module.css';
 
-export interface Props {}
-
-export const Layout: FunctionComponent<Props> = props => {
+export const Layout: FunctionComponent = props => {
     return (
         <>
             <Header />
