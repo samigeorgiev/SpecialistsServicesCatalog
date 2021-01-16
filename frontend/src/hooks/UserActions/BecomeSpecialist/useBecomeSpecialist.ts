fd95741c0a112cd96b8cfb31337f5575438606a9
Samuil Georgiev <sami.georgiev@gmail.com>
2021-01-16 09:54:33 +0200

    Finish

--- frontend/src/hooks/UserActions/BecomeSpecialist/useBecomeSpecialist.ts
+++ frontend/src/hooks/UserActions/BecomeSpecialist/useBecomeSpecialist.ts
@@ -3,13 +3,11 @@ import { HttpOptions } from '../../Http/HttpOptions';
 import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../../contexts/User/UserContext';
 import { BecomeSpecialist } from './BecomeSpecialist';
-import { useHistory } from 'react-router-dom';
 
 export const useBecomeSpecialist = (): BecomeSpecialist => {
     const [finished, setFinished] = useState(false);
     const { user, setUser } = useContext(UserContext);
     const { state, sendRequest } = useHttp<void>();
-    const history = useHistory();
 
     useEffect(() => {
         if (state.response) {
@@ -18,9 +16,8 @@ export const useBecomeSpecialist = (): BecomeSpecialist => {
             }
             setUser({ ...user, isSpecialist: true });
             setFinished(true);
-            history.push('/');
         }
-    }, [setUser, state.response, user, history]);
+    }, [setUser, state.response, user]);
 
     const doBecomeSpecialist = (): void => {
         if (user === null) {
