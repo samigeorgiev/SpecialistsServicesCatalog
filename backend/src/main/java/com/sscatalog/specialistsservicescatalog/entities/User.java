17ce905cc79fa8bea4ac6ae75bc6ae9aa99c2338
Samuil Georgiev <sami.georgiev@gmail.com>
2021-02-05 10:23:08 +0200

    reformat

--- backend/src/main/java/com/sscatalog/specialistsservicescatalog/entities/User.java
+++ backend/src/main/java/com/sscatalog/specialistsservicescatalog/entities/User.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
 
