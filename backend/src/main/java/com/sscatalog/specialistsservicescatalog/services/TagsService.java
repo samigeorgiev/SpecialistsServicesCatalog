fe79943173e8269f8cdff28b9dc3e4800f3f1315
Samuil Georgiev <sami.georgiev@gmail.com>
2021-02-07 10:30:20 +0200

    Optimize imports and reformat the code (#7)

--- backend/src/main/java/com/sscatalog/specialistsservicescatalog/services/TagsService.java
+++ backend/src/main/java/com/sscatalog/specialistsservicescatalog/services/TagsService.java
@@ -6,10 +6,8 @@ import com.sscatalog.specialistsservicescatalog.repositories.TagRepository;
 import com.sscatalog.specialistsservicescatalog.utils.DtoConverter;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Service
